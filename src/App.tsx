76cb2c9271d17a0456c1a7e4b9e9d628392ea0d0
Dmitrii Dementev <dmitrii.dementev@aisa.ru>
2024-06-05 22:12:10 +0300

    github pages

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,6 @@ import { useState } from 'react';
 function App() {
   const {
     counter_value,
-    recent_sender,
-    owner_address,
     contract_address,
     contract_balance,
     sendIncrement,
